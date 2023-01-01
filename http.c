ea22b05bc052a78c8e3bcbb5ebe435776cd771b2
calebTree <ande4905@gmail.com>
2022-12-31 20:28:33 -0600

    simple http client

--- http.c
+++ http.c
@@ -39,3 +39,28 @@ int parse_url(char *uri, char **host, char **path) {
     }
     return 0;
 }
+
+#define HTTP_PORT   80
+
+/**
+* Simple command-line HTTP client.
+*/
+int main(int argc, char * argv[]) {
+    int client_connection;
+    char *host, *path;
+    struct hostent *host_name;
+    struct sockaddr_in host_address;
+#ifdef WIN32
+    WSADATA wsaData;
+#endif // WIN32
+
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s: <URL>\n", argv[0]);
+        return 1;
+    }
+    if(parse_url(argv[1], &host, &path) == -1) {
+        fprintf(stderr, "Error - malformed URL '%s'.\n", argv[1]);
+        return 1;
+    }
+    printf("Connecting to host '%s'\n", host);
+}
