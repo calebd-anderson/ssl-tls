c66e1006c6882b9310b4d3e75c56622e5d91db16
calebTree <ande4905@gmail.com>
2023-01-14 09:24:49 -0600

    proxy support phase 1

--- http.c
+++ http.c
@@ -46,10 +46,15 @@ int parse_url(char *uri, char **host, char **path) {
 * on success. -1 on failure, with errno set appropriately. The caller
 * must then retrieve the response.
 */
-int http_get(int connection, const char *path, const char *host) {
+int http_get(int connection, const char *path, const char *host, const char *proxy_host, const char *proxy_user, const char *proxy_password) {
     static char get_command[MAX_GET_COMMAND];
+    if(proxy_host)
+    {
+        sprintf(get_command, "GET http://%s/%s HTTP/1.1\r\n", host, path);
+    } else {
+        sprintf(get_command, "GET /%s HTTP/1.1\r\n", path);
+    }
 
-    sprintf(get_command, "GET /%s HTTP/1.1\r\n", path);
     if(send(connection, get_command, strlen(get_command), 0) == -1) {
         return -1;
     }
@@ -85,27 +90,89 @@ void display_result(int connection) {
 
 #define HTTP_PORT   80
 
+int parse_proxy_param(char *proxy_spec, char **proxy_host, int *proxy_port, char **proxy_user, char **proxy_password) {
+    char *login_sep, *colon_sep, *trailer_sep;
+    // Technically the user should start the proxy spec with
+    // "http://". But, be forgiving if he didn't.
+    if(!strncmp("http://", proxy_spec, 7)) {
+        proxy_spec += 7;
+    }
+
+    login_sep = strchr(proxy_spec, '@');
+
+    if(login_sep) {
+        colon_sep = strchr(proxy_spec, ':' );
+        if(!colon_sep || (colon_sep> login_sep)) {
+            // Error - if username supplied, password must be supplied.
+            fprintf(stderr, "Expected password in '%s'\n", proxy_spec);
+            return 0;
+        }
+        *colon_sep = '\0';
+        *proxy_user = proxy_spec;
+        *login_sep = '\0';
+        *proxy_password = colon_sep + 1;
+        proxy_spec = login_sep + 1;
+    }
+
+    // If the user added a "/" on the end (as they sometimes do),
+    // just ignore it.
+    trailer_sep = strchr(proxy_spec, '/');
+    if(trailer_sep) {
+        *trailer_sep = '\0';
+    }
+
+    colon_sep = strchr(proxy_spec, ':');
+    if(colon_sep) {
+        // non-standard proxy port
+        *colon_sep = '\0';
+        *proxy_host = proxy_spec;
+        *proxy_port = atoi(colon_sep + 1);
+        if(*proxy_port == 0) {
+            // 0 is not a valid port; this is an error, whether
+            // it was mistyped or specified as 0.
+            return 0;
+        }
+    } else {
+        *proxy_port = HTTP_PORT;
+        *proxy_host = proxy_spec;
+    }
+    return 1;
+}
+
 /**
 * Simple command-line HTTP client.
 */
 int main(int argc, char * argv[]) {
     int client_connection;
+    char *proxy_host, *proxy_user, *proxy_password;
+    int proxy_port;
     char *host, *path;
     struct hostent *host_name;
     struct sockaddr_in host_address;
+    int ind;
 #ifdef WIN32
     WSADATA wsaData;
 #endif // WIN32
 
     if(argc < 2) {
-        fprintf(stderr, "Usage: %s: <URL>\n", argv[0]);
+        fprintf(stderr, "Usage: %s: [-p http://[username:password@]proxy-host:proxy-port]<URL>\n", argv[0]);
         return 1;
     }
-    if(parse_url(argv[1], &host, &path) == -1) {
+    proxy_host = proxy_user = proxy_password = host = path = NULL;
+    ind = 1;
+    if(!strcmp("-p", argv[ind]))
+    {
+        if(!parse_proxy_param(argv[++ind], &proxy_host, &proxy_port, &proxy_user, &proxy_password)) {
+            fprintf(stderr, "Error - malformed proxy parameter '%s'.\n", argv[2]);
+            return 2;
+        }
+        ind++;
+    }
+    if(parse_url(argv[ind], &host, &path) == -1) {
         fprintf(stderr, "Error - malformed URL '%s'.\n", argv[1]);
         return 1;
     }
-    printf("Connecting to host '%s'\n", host);
+
 
     // Step 1: open a socket connection on http port with the destination host.
     #ifdef WIN32
@@ -121,14 +188,21 @@ int main(int argc, char * argv[]) {
         return 2;
     }
 
-    host_name = gethostbyname(host);
+    if(proxy_host) {
+        printf("Connecting to host '%s'\n", proxy_host);
+        host_name = gethostbyname(proxy_host);
+    } else {
+        printf("Connecting to host '%s'\n", host);
+        host_name = gethostbyname(host);
+    }
+
     if(!host_name) {
         perror("Error in name resolution");
         return 3;
     }
 
     host_address.sin_family = AF_INET;
-    host_address.sin_port = htons(HTTP_PORT);
+    host_address.sin_port = htons(proxy_host ? proxy_port : HTTP_PORT);
     memcpy(&host_address.sin_addr, host_name->h_addr_list[0], sizeof(struct in_addr));
 
     if(connect(client_connection, (struct sockaddr *) &host_address, sizeof(host_address)) == -1) {
@@ -138,7 +212,7 @@ int main(int argc, char * argv[]) {
 
     printf("Retrieving document: '%s'\n", path);
 
-    http_get(client_connection, path, host);
+    http_get(client_connection, path, host, proxy_host, proxy_user, proxy_password);
 
     display_result(client_connection);
 
