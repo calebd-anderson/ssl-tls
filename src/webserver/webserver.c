ffb519b546198e3884981514ff7ba58dbd982d95
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-10-11 19:59:08 -0500

    redo includes in webserver match http client

--- src/webserver/webserver.c
+++ src/webserver/webserver.c
@@ -1,30 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
 #ifdef WIN32
 #include <winsock2.h>
-#include <stdio.h>
 #include <windows.h>
 #else
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <netlinet/in.h>
 #include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <stdarg.h>
-/* the next two includes probably aren't relevant for you, but I typically use them all anyway */
-#include <math.h>
-#include <sys/termios.h>
-#endif
+#endif // WIN32
 
 #define HTTP_PORT 80
 
