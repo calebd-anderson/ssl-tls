bc2cd19ca9556d683bb944849e0dec315a753e53
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-10-22 12:50:43 -0500

    add expansion_table

--- src/des/des.c
+++ src/des/des.c
@@ -108,3 +108,15 @@ static void ro1( unsigned char *target )
     target[5] = (target[5] << 1) | ((target[6] & 0x80) >> 7);
     target[6] = (target[6] << 1) | carry_right;
 }
+
+// Listing 2-10: expansion table
+static const int expansion_table[] = {
+    32,  1, 2, 3, 4, 5,
+    4,   5, 6, 7, 8, 9,
+    8,   9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1
+};
