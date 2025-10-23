faf69f0daf08c82719f8c0fb299e9977aecdbfb8
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-10-23 07:27:32 -0500

    add xor to des header

--- src/des/des.h
+++ src/des/des.h
@@ -1,10 +1,13 @@
 #ifndef DES_H
 #define DES_H
 
+void xor(unsigned char *target, const unsigned char *src, int len);
+
 void permute(unsigned char target[],
                     const unsigned char src[],
                     const int permute_table[],
                     int len);
+
 void ro1( unsigned char *target );
 
 #endif
